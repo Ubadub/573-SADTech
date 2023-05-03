591f227fb26bb91ece5046f9318cc35b1c39bc94
limele <574.electronic.utility@gmail.com>
2023-05-03 11:11:19 -0700

    Added classifiers for Logistic Regression and Stochastic Gradient Descent.

---
+++ src/classifiers/stochastic_gradient_descent_classifier.py
@@ -0,0 +1,50 @@
+"""
+    Stochastic Gradient Descent Classifier
+"""
+
+# from typing import *
+
+import datasets
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+
+from classifiers import Classifier
+
+
+class StochasticGradientDescentClassifier(Classifier):
+    def __init__(self, config: dict, ds_dict: datasets.DatasetDict) -> None:
+        """
+        Params:
+            - config: a configuration .yml file
+            - ds_dict: a huggingface datasets object containing the train data
+
+        Initializes a Stochastic Gradient Descent Classifier Model
+        """
+        super().__init__(config=config, ds_dict=ds_dict)
+        self.model = SGDClassifier()
+
+    def train_predict(
+        self, train_indices: np.ndarray, dev_indices: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Params:
+            - train_indices: A list of indices corresponding to the files used for training
+            - dev_indices: A list of indices corresponding to the files used for evaluating
+
+        This method trains the Stochastic Gradient Descent Classifier using the given train data, and also predicts the labels
+            of the given Development data
+
+        Returns:
+            - dev_gold_labels
+            - predicted
+        """
+        train_vectors = self.feature_vectors[train_indices]
+        dev_vectors = self.feature_vectors[dev_indices]
+
+        train_gold_labels = self.gold_labels[train_indices]
+        dev_gold_labels = self.gold_labels[dev_indices]
+
+        self.model.fit(train_vectors, train_gold_labels)
+        predicted = self.model.predict(np.array(dev_vectors))
+
+        return dev_gold_labels, predicted
