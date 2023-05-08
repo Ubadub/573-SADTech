6ba8f495ad89a9b692a8f285998b30a1ece6990d
Abhinav Patil <abhinavp@uw.edu>
2023-05-08 15:32:36 -0700

    Amended pipeline transformers to use Hydra configs

--- src/pipeline_transformers/__main__.py
+++ src/pipeline_transformers/__main__.py
@@ -7,9 +7,13 @@ from typing import Optional, Union
 
 from imblearn.pipeline import Pipeline
 
+from datasets import Dataset, DatasetDict, load_from_disk
+
+import hydra
+
 import numpy as np
 
-from datasets import Dataset, DatasetDict, load_from_disk
+from omegaconf import DictConfig, OmegaConf
 
 from sklearn.base import BaseEstimator
 from sklearn.compose import ColumnTransformer
@@ -18,19 +22,28 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
+from sklearn.utils.validation import check_is_fitted
 
-import yaml
+# from common import GLOBAL_SEED
 
-from common import GLOBAL_SEED
+from pipeline_transformers.utils import setup_rng
 
-np.random.seed(GLOBAL_SEED)
+# np.random.seed(GLOBAL_SEED)
+
+# from . import get_or_create_rng
+
+
+CFG_MODEL_SAVE_PATH = "model_save_path"
 
 CFG_CLASSIFIER_CACHE = "ClassifierCachePath"
 
 CFG_TEXT_TRANSFORMERS = "TextTransformers"
 CFG_AUDIO_TRANSFORMERS = "AudioTransformers"
+CFG_TRANSFORMERS = "transformers"
 CFG_CLASSIFIER = "Classifier"
-CFG_RESAMPLERS = "Resamplers"
+CFG_PRERESAMPLE_TRANSFORMERS = "preresample_transformers"
+CFG_RESAMPLERS = "resamplers"
+CFG_POSTRESAMPLE_TRANSFORMERS = "postresample_transformers"
 CFG_NAME = "name"
 CFG_CLASS = "class"
 CFG_ARGS = "args"
@@ -39,8 +52,8 @@ CFG_KWARGS = "kwargs"
 TEXT_COL = "text"
 AUDIO_COL = "audio"
 Y_COL = "label"
-ALL_FEATS = [AUDIO_COL, TEXT_COL]
-# ALL_FEATS = [TEXT_COL]
+# ALL_FEATS = [AUDIO_COL, TEXT_COL]
+ALL_FEATS = [TEXT_COL]
 
 PARSER_CONFIG = {
     # "prog": "python -m transformer_lm",
@@ -58,7 +71,7 @@ SUBPARSERS_CONFIG = {
 
 
 # def crossfold(clf: BaseEstimator, ds: Dataset, do_fit=True, saved_models_root_path=None):
-def crossfold(clf_or_saved_path: Union[str, BaseEstimator], ds: Dataset):
+def crossfold(clf_or_saved_path: Union[str, BaseEstimator], ds: Dataset, n_splits: int):
     # if not do_fit and saved_models_root_path is None:
     #     sys.exit("Need to refit models or pass path to existing models.")
 
@@ -69,7 +82,7 @@ def crossfold(clf_or_saved_path: Union[str, BaseEstimator], ds: Dataset):
     else:
         sys.exit("Need to provide classifier or path to saved classifiers.")
 
-    skfolds = StratifiedKFold(n_splits=4)
+    skfolds = StratifiedKFold(n_splits=n_splits)
 
     y_true_pooled = []
     y_pred_pooled = []
@@ -79,8 +92,8 @@ def crossfold(clf_or_saved_path: Union[str, BaseEstimator], ds: Dataset):
     ):
         # if n > 0:
         #     sys.exit(0)
-        # print(f"train_idxs: {train_idxs}")
-        # print(f"eval_idxs: {eval_idxs}")
+        print(f"train_idxs: {train_idxs}")
+        print(f"eval_idxs: {eval_idxs}")
         # train_ds = rebalance_ds(ds.select(train_idxs), seed=GLOBAL_SEED, shuffle=True, shuffle_seed=GLOBAL_SEED)
         train_ds = ds.select(train_idxs)
         eval_ds = ds.select(eval_idxs)
@@ -111,165 +124,144 @@ def crossfold(clf_or_saved_path: Union[str, BaseEstimator], ds: Dataset):
     print(classification_report(y_true_pooled, y_pred_pooled))
 
 
-def train(cfg_path: str, dataset_path: str, save_model_to: Optional[str] = None):
-    with open(cfg_path, "r") as ymlfile:
-        cfg = yaml.unsafe_load(ymlfile)
+def train(
+    pipeline_cfg: DictConfig,
+    ds: Dataset,
+    n_splits: int,
+    # default_cache_path = f"sklearn_cache/{os.path.basename(cfg_path)}/classifier"
+    # classifier_cache = cfg.get(CFG_CLASSIFIER_CACHE, default_cache_path)
+    # model_save_path = pipeline_cfg.get(CFG_MODEL_SAVE_PATH)
+):
+    text_preprocessors = pipeline_cfg.text
+    audio_preprocessors = pipeline_cfg.audio
 
-    default_cache_path = f"sklearn_cache/{os.path.basename(cfg_path)}/classifier"
-    classifier_cache = cfg.get(CFG_CLASSIFIER_CACHE, default_cache_path)
-
-    ds_dict: DatasetDict = load_from_disk(dataset_path)
-    ds: Dataset = ds_dict["train"]
-
-    text_transformers_cfg = cfg.get(CFG_TEXT_TRANSFORMERS, [])
+    # text_transformers_cfg = pipeline_cfg.get(CFG_TEXT_TRANSFORMERS, [])
     text_transformer = Pipeline(
         steps=[
-            (
-                tr[CFG_NAME],
-                tr[CFG_CLASS](*tr.get(CFG_ARGS, []), **tr.get(CFG_KWARGS, {})),
-            )
-            for tr in text_transformers_cfg
+            ("vectorizer", text_preprocessors.vectorizer),  # mandatory
+            *(
+                (tr_name, tr)
+                for tr_name, tr in text_preprocessors.get(CFG_TRANSFORMERS, {}).items()
+            ),
         ],
         # memory="sklearn_cache/text_transformer",
     )
 
-    print("Text transformer:", text_transformer)
+    # audio_transformer = Pipeline(
+    #     steps=[
+    #         audio_preprocessors.vectorizer,  # mandatory
+    #         *(
+    #             (tr_name, tr)
+    #             for tr_name, tr in audio_preprocessors.get(CFG_TRANSFORMERS, {}).items()
+    #         ),
+    #     ],
+    # )
 
-    audio_transformers_cfg = cfg.get(CFG_AUDIO_TRANSFORMERS, [])
-    audio_transformer = Pipeline(
-        steps=[
-            (
-                tr[CFG_NAME],
-                tr[CFG_CLASS](*tr.get(CFG_ARGS, []), **tr.get(CFG_KWARGS, {})),
-            )
-            for tr in audio_transformers_cfg
-        ],
-    )
+    print("Text transformer:", text_transformer)
+    # print("Audio transformer:", audio_transformer)
 
     preprocessor = ColumnTransformer(
         transformers=[
-            ("text", text_transformer, TEXT_COL),
-            ("audio", audio_transformer, AUDIO_COL),
+            (TEXT_COL, text_transformer, TEXT_COL),
+            # (AUDIO_COL, audio_transformer, AUDIO_COL),
         ],
         n_jobs=-1,
     )
 
     print("Preprocessor:", preprocessor)
 
+    preresample_transformers = [
+        (tr_name, tr)
+        for tr_name, tr in pipeline_cfg.get(CFG_PRERESAMPLE_TRANSFORMERS, {}).items()
+    ]
+
+    print("Preresample transformers:", preresample_transformers)
+
     resamplers = [
-        (
-            resampler_cfg[CFG_NAME],
-            resampler_cfg[CFG_CLASS](
-                *resampler_cfg.get(CFG_ARGS, []),
-                **resampler_cfg.get(CFG_KWARGS, {}),
-            ),
-        )
-        for resampler_cfg in cfg.get(CFG_RESAMPLERS, [])
+        (resampler_name, resampler)
+        for resampler_name, resampler in pipeline_cfg.get(CFG_RESAMPLERS, {}).items()
     ]
 
     print("Resamplers:", resamplers)
 
-    classifier_cfg = cfg.get(CFG_CLASSIFIER, {})
-    classifier = (
-        "classifier",
-        classifier_cfg[CFG_CLASS](
-            *classifier_cfg.get(CFG_ARGS, []),
-            **classifier_cfg.get(CFG_KWARGS, {}),
-        ),
-    )
+    postresample_transformers = [
+        (tr_name, tr)
+        for tr_name, tr in pipeline_cfg.get(CFG_POSTRESAMPLE_TRANSFORMERS, {}).items()
+    ]
+
+    print("Postresample transformers:", postresample_transformers)
+
+    classifier = pipeline_cfg.classifier  # mandatory
 
     print("Classifier:", classifier)
 
     clf = Pipeline(
         steps=[
             ("preprocessor", preprocessor),
+            *preresample_transformers,
             *resamplers,
-            classifier,
+            *postresample_transformers,
+            ("classifier", classifier),
         ],
         # memory=classifier_cache,
-        # ("classifier", LogisticRegression())]
     )
 
     print("Pipeline:", clf)
+    for n, fitted_clf in enumerate(
+        crossfold(clf_or_saved_path=clf, ds=ds, n_splits=n_splits)
+    ):
+        print(f"Finished fold {n}")
+    #     if model_save_path is not None:
+    #         out_path = os.path.join(model_save_path, f"{n}.pkl")
+    #         with open(out_path, "wb") as f:
+    #             pickle.dump(fitted_clf, f)
+    #             print("Saved model to:", out_path)
 
-    for n, fitted_clf in enumerate(crossfold(clf_or_saved_path=clf, ds=ds)):
-        if save_model_to is not None:
-            out_path = os.path.join(save_model_to, f"{n}.pkl")
-            with open(out_path, "wb") as f:
-                pickle.dump(fitted_clf, f)
-                print("Saved model to:", out_path)
 
-
-def infer(dataset_path: str, saved_models_dir: str):
-    ds_dict: DatasetDict = load_from_disk(dataset_path)
-    ds: Dataset = ds_dict["train"]
+def infer(ds: Dataset, saved_models_dir: str):
     for _ in crossfold(clf_or_saved_path=saved_models_dir, ds=ds):
         pass
     # crossfold(clf_or_path=saved_models_dir, ds=ds, do_fit=True)
 
 
-def main():
-    parser = ArgumentParser(**PARSER_CONFIG)
-    subparsers = parser.add_subparsers(**SUBPARSERS_CONFIG)
+# @hydra.main( version_base=None, config_path="../config/hydra_root", config_name="config")
+@hydra.main(version_base=None, config_path="../config/hydra_root")
+def main(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg, resolve=False))
+    # print(OmegaConf.to_yaml(cfg, resolve=True))
+    cfg = setup_rng(cfg)
+    # print(cfg.global_rng is cfg.pipeline.classifier.random_state)
+    # assert cfg.global_rng is cfg.pipeline.classifier.random_state
+    # print(OmegaConf.to_yaml(cfg))
 
-    parser.add_argument(
-        "-d",
-        "--dataset",
-        required=True,
-        # default="../../data/train_dataset_dict",
-        dest="dataset_path",
-        metavar="PATH/TO/DATASET/",
-        help="Path to the dataset.",
-    )
+    is_debug = cfg.debug
 
-    inf_subparser = subparsers.add_parser(
-        "infer", aliases=["i", "inf"], description="Do inference"
-    )
-    inf_subparser.add_argument(
-        "-m",
-        "--saved_model_path",
-        required=True,
-        dest="saved_models_dir",
-        metavar="PATH/TO/SAVED/CLASSIFIERS",
-        help="Path where the saved per-fold classifiers are.",
-    )
-    # inf_subparser.add_argument(
-    #     "-l",
-    #     "--lang",
-    #     required=True,
-    #     choices=["tam", "mal"],
-    #     help="Language (tam for Tamil, mal for Malayalam)",
-    # )
-    inf_subparser.set_defaults(func=infer)
+    # Path to saved datasets
+    ds_path: str = hydra.utils.to_absolute_path(cfg.dataset)
 
-    train_subparser = subparsers.add_parser(
-        "train",
-        aliases=["t", "tr", "train", "f", "finetune"],
-        description="Finetune a pretrained model from HuggingFace",
-    )
-    train_subparser.add_argument(
-        "-c",
-        "--config",
-        required=True,
-        dest="cfg_path",
-        metavar="PATH/TO/CONFIG.YML",
-        help="Path to the config YAML file for this train run.",
-    )
-    train_subparser.add_argument(
-        "-m",
-        "--save_model_to",
-        default=None,
-        dest="save_model_to",
-        metavar="PATH/TO/SAVE/CLASSIFIERS",
-        help="Path where to save the per-fold classifiers.",
-    )
-    train_subparser.set_defaults(func=train)
+    ds_dict: DatasetDict = load_from_disk(ds_path)
+    ds: Dataset = ds_dict["train"]
 
-    args = parser.parse_args()
-    func_kwargs = dict(vars(args))
-    del func_kwargs["func"]
+    # train(cfg, ds, model_save_path=cfg.model_save_path)
 
-    args.func(**func_kwargs)
+    pipeline_cfg = hydra.utils.instantiate(cfg.pipeline)  # , _recursive_=False)
+    # if "_target_" in pipeline_cfg: # do inference only
+    if isinstance(pipeline_cfg, BaseEstimator) and check_is_fitted(
+        pipeline_cfg
+    ):  # do inference only
+        print("Inference.")
+        # Path where the saved per-fold classifiers are.
+        model_save_path: str = hydra.utils.to_absolute_path(
+            pipeline_cfg.model_save_path
+        )
+    elif isinstance(pipeline_cfg, BaseEstimator):  # TODO
+        sys.exit("Presently, directly instantiated pipelines must already be fitted")
+    else:
+        # do training
+        print("Training.")
+        train(pipeline_cfg, ds, cfg.n_splits)
+
+    # args.func(**func_kwargs)
 
 
 if __name__ == "__main__":
