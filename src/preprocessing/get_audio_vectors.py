b6324dbcf9157053ff0af80188e276dd0aaaa5bd
Tara Wueger <taraw28@uw.edu>
2023-05-07 19:50:59 -0700

    started working on mctct vectors but installing torchaudio is not working

--- src/preprocessing/get_audio_vectors.py
+++ src/preprocessing/get_audio_vectors.py
@@ -7,7 +7,7 @@ import numpy as np
 
 import datasets
 
-from transformers import Wav2Vec2FeatureExtractor, ClapFeatureExtractor
+from transformers import Wav2Vec2FeatureExtractor, ClapFeatureExtractor, MCTCTFeatureExtractor
 
 
 def get_wav2vec2_features(audio_array: list[np.ndarray], file_names: list[str]) -> dict[str, np.ndarray]:
@@ -16,7 +16,7 @@ def get_wav2vec2_features(audio_array: list[np.ndarray], file_names: list[str]) 
         - audio_array: list of arrays representations of audio files
         - file_names: list of file names in the current dataset
 
-    Creates wav2vec2 feature vectors for each audio array.
+    Creates and returns wav2vec2 feature vectors for each audio array.
     From: https://huggingface.co/docs/transformers/v4.28.1/en/model_doc/wav2vec2#transformers.Wav2Vec2FeatureExtractor
 
     Returns:
@@ -36,13 +36,35 @@ def get_clap_features(audio_array: list[np.ndarray], file_names: list[str]) -> d
         - audio_array: list of arrays representations of audio files
         - file_names: list of file names in the current dataset
 
-    Creates mel-filter bank feature vectors for each audio array.
+    Creates and returns mel-filter bank feature vectors for each audio array.
     From: https://huggingface.co/docs/transformers/model_doc/clap#transformers.ClapFeatureExtractor
 
     Returns:
         - A dictionary that maps each audio file name to its feature vector.
     """
     feature_extractor = ClapFeatureExtractor(sampling_rate=16000)
+    inputs = feature_extractor(audio_array,
+                               return_tensors="np",
+                               sampling_rate=16000,
+                               padding=True)["input_features"]
+
+    return get_vector_dict(inputs, file_names)
+
+
+# Needs torchaudio library
+def get_mctct_features(audio_array: list[np.ndarray], file_names: list[str]) -> dict[str, np.ndarray]:
+    """
+    Params:
+        - audio_array: list of arrays representations of audio files
+        - file_names: list of file names in the current dataset
+
+    Creates and returns M-CTC-T feature vectors for each audio array.
+    From: https://huggingface.co/docs/transformers/model_doc/mctct#transformers.MCTCTFeatureExtractor
+
+    Returns:
+        - A dictionary that maps each audio file name to its feature vector.
+    """
+    feature_extractor = MCTCTFeatureExtractor(sampling_rate=16000)
     inputs = feature_extractor(audio_array,
                                return_tensors="np",
                                sampling_rate=16000,
@@ -60,7 +82,7 @@ def get_vector_dict(inputs, file_names):
 
 def main():
     parser: argparse.ArgumentParser = argparse.ArgumentParser(
-        description="Create vectors from the audio files in the given language's data directory.",
+        description="Create vectors from the audio files in the given language's dataset.",
     )
     parser.add_argument("lang")
     parser.add_argument("-o", "--output")
@@ -80,6 +102,7 @@ def main():
 
     # vectors = get_wav2vec2_features(audio_array, file_names)
     # vectors = get_clap_features(audio_array, file_names)
+    # vectors = get_mctct_features(audio_array, file_names)
 
     # print(type(vectors))
 
