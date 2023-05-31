9285bb68484ac2fc5ae5467f6545d59bf1050317
briggs3 <briggzi31@github.com>
2023-05-31 16:27:53 -0700

    added output_path specification functionality

--- src/analytics/README.md
+++ src/analytics/README.md
@@ -1,6 +1,10 @@
-To run:
+To run Tamil (output_path optional):
 
 ```
-    python3 analytics/instance_counts.py mal
-    python3 analytics/instance_counts.py tam
+    python3 analytics/instance_counts.py tam (output_path)
+```
+
+To run Malayalam (output_path optional):
+```
+    python3 analytics/instance_counts.py mal (output_path)
 ```
--- src/analytics/instance_counts.py
+++ src/analytics/instance_counts.py
@@ -6,13 +6,16 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from typing import Optional
+
 AXIS_FONT_SIZE = 16
 
 
 def create_instance_count_plot(
         ds_dict: datasets.arrow_dataset.Dataset,
         lang: str,
-        stage: str
+        stage: str,
+        output_path: Optional[str] = "analytics"
 ) -> None:
     if lang == "mal":
         lang = "Malayalam"
@@ -38,18 +41,24 @@ def create_instance_count_plot(
     title = f"{lang} {stage} Data Class Frequency"
     plt.title(title, fontsize=AXIS_FONT_SIZE)
 
-    output_path = os.path.join("analytics", title)
+
+
+    output_path = os.path.join(output_path, title)
     plt.savefig(output_path, bbox_inches="tight")
 
 
 if __name__ == '__main__':
     lang = sys.argv[1]
-
     ds_dict_path = f"../data/{lang}/train_dataset_dict"
     ds_dict: datasets.DatasetDict = datasets.load_from_disk(ds_dict_path)
 
     ds_dict_train = ds_dict["train"]
     ds_dict_test = ds_dict["test"]
 
-    create_instance_count_plot(ds_dict_train, lang, "Train")
-    create_instance_count_plot(ds_dict_test, lang, "Test")
+    try:
+        output_path = sys.argv[2]
+        create_instance_count_plot(ds_dict_train, lang, "Train", output_path)
+        create_instance_count_plot(ds_dict_test, lang, "Test", output_path)
+    except:
+        create_instance_count_plot(ds_dict_train, lang, "Train")
+        create_instance_count_plot(ds_dict_test, lang, "Test")
