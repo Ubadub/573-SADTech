0032132b75e2948f3a2ebbe4771de3d0e74b206f
Abhinav Patil <abhinavp@uw.edu>
2023-06-01 15:08:56 -0700

    Reworked multiseed averaging script to work with new outputs/ directory structure

--- src/scripts/multiseed_averaging.py
+++ src/scripts/multiseed_averaging.py
@@ -36,39 +36,66 @@ def multiseed_average(root_dir: str) -> dict:
     root_dir = os.path.realpath(root_dir)
     d = infinite_defaultdict()
     for parent_path, child_dirs, child_files in os.walk(root_dir, topdown=False):
-        if child_files:
-            if child_dirs:
-                sys.exit(
-                    f"File(s) {child_files} nested at same level"
-                    f"as director(y/ies) {child_dirs}; aborting."
-                )
-            components = recurs_split(parent_path.removeprefix(root_dir))
-            curr_d = d
-            for component in components[:-1]:
-                curr_d = curr_d[component]
-            if not isinstance(curr_d[components[-1]], defaultdict):
-                sys.exit(f"Detected already initialized value at {parent_path}")
-            else:
-                files = [os.path.join(parent_path, c) for c in child_files]
-                scores = defaultdict(list)
-                for f in files:
-                    with open(f, "rb") as _:
-                        res_dict = pickle.load(file=_)
-                    report = classification_report(
-                        res_dict["y_true"],
-                        res_dict["y_pred"],
-                        output_dict=True,
-                        zero_division=0,
+        if child_dirs:
+            seed_check = [_.startswith("seed") for _ in child_dirs]
+            if all(seed_check):
+                if child_files:
+                    sys.exit(
+                        f"Path {parent_path} contains files nested at same level as"
+                        "seed folders; aborting."
                     )
-                    scores["macro_f1"].append(report["macro avg"]["f1-score"])
-                    scores["macro_prec"].append(report["macro avg"]["precision"])
-                    scores["macro_rec"].append(report["macro avg"]["recall"])
-                    scores["accuracy"].append(report["accuracy"])
-                curr_d[components[-1]] = {
-                    score_name: avg(score_list)
-                    # score_name: sum(score_list) / len(score_list)
-                    for score_name, score_list in scores.items()
-                }
+                components = recurs_split(parent_path.removeprefix(root_dir))
+                curr_d = d
+                for component in components[:-1]:
+                    curr_d = curr_d[component]
+                if not isinstance(curr_d[components[-1]], defaultdict):
+                    sys.exit(f"Detected already initialized value at {parent_path}")
+                else:
+                    files = [os.path.join(parent_path, c, "results.pkl") for c in child_dirs]
+                    scores = defaultdict(list)
+                    for f in files:
+                        with open(f, "rb") as _:
+                            res_dict = pickle.load(file=_)
+                        dev_report = classification_report(
+                            res_dict["y_true"],
+                            res_dict["y_pred"],
+                            output_dict=True,
+                            zero_division=0,
+                        )
+                        test_report = classification_report(
+                            res_dict["full_y_true"],
+                            res_dict["full_y_pred"],
+                            output_dict=True,
+                            zero_division=0,
+                        )
+                        scores["dev_macro_f1"].append(dev_report["macro avg"]["f1-score"])
+                        scores["dev_macro_prec"].append(dev_report["macro avg"]["precision"])
+                        scores["dev_macro_rec"].append(dev_report["macro avg"]["recall"])
+                        scores["dev_accuracy"].append(dev_report["accuracy"])
+
+                        scores["test_macro_f1"].append(test_report["macro avg"]["f1-score"])
+                        scores["test_macro_prec"].append(test_report["macro avg"]["precision"])
+                        scores["test_macro_rec"].append(test_report["macro avg"]["recall"])
+                        scores["test_accuracy"].append(test_report["accuracy"])
+                    curr_d[components[-1]] = {
+                        score_name: avg(score_list)
+                        # score_name: sum(score_list) / len(score_list)
+                        for score_name, score_list in scores.items()
+                    }
+
+            elif any(seed_check):
+                sys.exit(
+                    f"Dir(s) {child_dirs} contains mix of seed/non-seed dirs; aborting."
+                )
+            # if child_files:
+            #     parent_path_base_name = os.path.basename(parent_path)
+            #     if parent_path_base_name.startswith("seed"):
+            #         parent_path_dir_name = os.path.dirname(parent_path)
+            #     if child_dirs:
+            #         sys.exit(
+            #             f"File(s) {child_files} nested at same level"
+            #             f"as director(y/ies) {child_dirs}; aborting."
+            #         )
 
     return flatten_default_dict(d)
 
